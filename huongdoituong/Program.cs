379dce587cc102c3080c700a7bdf85b2226c48e7
huyhau823@gmail.com <huyhau823@gmail.com>
2023-09-22 08:35:30 +0700

    Add project files.

---
+++ huongdoituong/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace huongdoituong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            thuvien TV = new thuvien();
+            // Thêm sách vào thư viện
+            Sach sach1 = new Sach(1, "Cuốn theo chiều gió", "Margaret Mitchell");
+            TV.ThemSach(sach1);
+
+            Sach sach2 = new Sach(2, "Harry Potter và Hòn đá Phù thủy", "J.K. Rowling");
+            TV.ThemSach(sach2);
+
+            // Hiển thị danh sách sách
+            TV.HienThiDanhSachSach();
+
+            // Xoá sách khỏi thư viện
+            TV.XoaSach(1);
+
+            // Hiển thị danh sách sách sau khi xoá
+            TV.HienThiDanhSachSach();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ huongdoituong/sach.cs
@@ -0,0 +1,70 @@
+﻿using huongdoituong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace huongdoituong
+{
+    internal class Sach
+    {
+        public int MaSach { get; set; }
+        public string TenSach { get; set; }
+        public string TacGia { get; set; }
+
+        public Sach() { }
+
+
+        public Sach(int maSach, string tenSach, string tacGia)
+        {
+            MaSach = maSach;
+            TenSach = tenSach;
+            TacGia = tacGia;
+        }
+        public void hienthi()
+        {
+            Console.WriteLine("ma sach :" + MaSach + "    ten sach :" + TenSach + "      tac gia :" + TacGia);
+        }
+    }
+
+
+
+    class thuvien
+    {
+        
+        List<Sach> ds1 = new List<Sach>();
+        public void ThemSach(Sach SACH)
+        {           
+           ds1.Add(SACH);
+            Console.WriteLine("sach da duoc them vao thu vien.");
+            Console.WriteLine("");
+        }
+        public void XoaSach(int maSach)
+        {
+            Sach sachxoa = ds1.Find(s => s.MaSach == maSach);
+            if (sachxoa != null)
+            {
+                ds1.Remove(sachxoa);
+                Console.WriteLine("sach da bi xoa.");
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("khong tim thay sach");
+                Console.WriteLine("");
+            }
+        }
+        public void HienThiDanhSachSach()
+        {
+            Console.WriteLine("danh sach trong thu vien:");
+            
+            foreach (Sach SACH in ds1)
+            {
+                Console.WriteLine(SACH);
+            }
+        }
+    }
+}
+
+
